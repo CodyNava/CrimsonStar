828b4e98b9f7049a12af1954037eda5bdbdc1692
Marc Hoffmann <m.h.96@live.de>
2025-04-08 14:21:29 +0200

    [+] Added ScriptableObject Classes for ShipModules

---
+++ Assets/01_Scripts/Ship/Modules/BaseShipModule.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _01_Scripts.Ship.Modules
+{
+    public abstract class BaseShipModule : ScriptableObject
+    {
+        
+        public float health;
+        public float weight;
+    }
+}
---
+++ Assets/01_Scripts/Ship/Modules/TurretModule.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Object = System.Object;
+
+namespace _01_Scripts.Ship.Modules
+{
+    [CreateAssetMenu(fileName = "TurretModule", menuName = "ShipModules/TurretModule", order = 1)]
+    public class TurretModule : BaseShipModule
+    {
+        public float reloadTime;
+        public Object projectile;
+    }
+}
