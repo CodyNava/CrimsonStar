5e1381f7fae3384ee8886048dc1cdabed927ed7a
Marc Hoffmann <m.h.96@live.de>
2025-04-08 15:26:23 +0200

    [+] Added Projectile scriptableObjects

---
+++ Assets/01_Scripts/Projectiles/BaseProjectileObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _01_Scripts.Projectiles
+{
+    public class BaseProjectileObject : ScriptableObject
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float damage;
+
+    }
+}
