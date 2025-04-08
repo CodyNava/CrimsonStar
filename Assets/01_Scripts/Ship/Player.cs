3869c437d7bda25b1972c70c10d4b0028962538c
Martin <martin.mann.t@gmail.com>
2025-04-08 16:29:19 +0200

    Player movement adjustment
    changed to W/S = foreward backward, A/D rotating player

--- Assets/01_Scripts/Ship/Player.cs
+++ Assets/01_Scripts/Ship/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     private Vector3 velocity;
     public float weight = 100f;
     public float maxSpeed = 20;
+    public float rotationSpeed = 180f;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -24,27 +25,26 @@ public class Player : MonoBehaviour
 
     public void movePlayer()
     {
-        Vector3 movement = new Vector3(input.x, input.y, 0).normalized;
+        Vector3 movement = (transform.up * input.y).normalized;
+
         velocity += moveSpeed * movement;
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-        
-        if (input.magnitude <= 0.0001)
+
+        if (input.magnitude <= 0.0001f)
         {
             velocity *= 1f - (weight / 1000f);
         }
-        
-        if (velocity.magnitude <= 0.0001)
+
+        if (velocity.magnitude <= 0.0001f)
         {
             velocity = Vector3.zero;
         }
+
         transform.Translate(velocity * Time.deltaTime, Space.World);
         
-        if (movement != Vector3.zero)
-        {
-            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90f;
-            Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.15f);
-        }
+        
+        
+        transform.Rotate(0f, 0f, -input.x * rotationSpeed * Time.deltaTime);
         
         
     }
