20d259451e504e153e0e95fa6e95e7ee2eee56ef
MikoHeine <Miko.Heine@games-academy.de>
2025-04-08 15:06:23 +0200

    Small Script changes

--- Assets/01_Scripts/ShipEditor/PartSpawner.cs
+++ Assets/01_Scripts/ShipEditor/PartSpawner.cs
@@ -6,17 +6,19 @@ public class PartSpawner : MonoBehaviour
     public GameObject partParent;
     public Transform SpawnPosition;
     public Snap snap;
-    public ShipEditorCurrencySystem currencySystem;
 
     public void SpawnPart(int index)
     {
         if (partPrefab != null && index >= 0 && index < partPrefab.Length)
         {
-            GameObject newPart = Instantiate(partPrefab[index], SpawnPosition.transform.position, transform.rotation);
+            GameObject newPart = Instantiate(
+                partPrefab[index],
+                SpawnPosition.position,
+                Quaternion.Euler(0, 0, 30)
+            );
 
             newPart.transform.SetParent(partParent.transform);
             snap._dragscripts.Add(newPart.GetComponent<Drag>());
-            //currencySystem.currency -= partPrefab[index].GetComponent<Part>().cost;
         }
     }
 
