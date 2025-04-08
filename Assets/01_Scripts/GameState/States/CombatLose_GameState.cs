1fb31aefbec8a18580e5a7d6ee6e50daf923a382
Marc Hoffmann <m.h.96@live.de>
2025-04-08 13:59:53 +0200

    [+] Added Win & Lose GameStates

---
+++ Assets/01_Scripts/GameState/States/CombatLose_GameState.cs
@@ -0,0 +1,20 @@
+﻿namespace _01_Scripts.GameState.States
+{
+    public class CombatLose_GameState : GameState<CombatLose_GameState>
+    {
+        protected override void OnStateEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnStateUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnStateExit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/01_Scripts/GameState/States/CombatWin_GameState.cs
@@ -0,0 +1,20 @@
+﻿namespace _01_Scripts.GameState.States
+{
+    public class CombatWin_GameState : GameState<CombatWin_GameState>
+    {
+        protected override void OnStateEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnStateUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnStateExit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
