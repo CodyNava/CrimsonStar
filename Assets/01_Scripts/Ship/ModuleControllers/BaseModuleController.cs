0d21a59b139ab57bf8e4a3f5e98b272ca879c2d9
Marc Hoffmann <m.h.96@live.de>
2025-04-08 15:25:56 +0200

    [+] Added ModuleControllers
    Added controller scripts for specialized modules

---
+++ Assets/01_Scripts/Ship/ModuleControllers/BaseModuleController.cs
@@ -0,0 +1,33 @@
+﻿using _01_Scripts.GameState;
+using _01_Scripts.GameState.States;
+using UnityEngine;
+
+namespace _01_Scripts.Ship.ModuleControllers
+{
+    public class BaseModuleController : MonoBehaviour
+    {
+        private bool _isCombatActive;
+        
+        public void Awake()
+        {
+            Combat_GameState.onEnterState += OnEnterCombatState;
+            Combat_GameState.onExitState += OnExitCombatState;
+        }
+
+        public void OnDestroy()
+        {
+            Combat_GameState.onEnterState -= OnEnterCombatState;
+            Combat_GameState.onExitState -= OnExitCombatState;
+        }
+
+        private void OnExitCombatState()
+        {
+            _isCombatActive = false;
+        }
+        
+        private void OnEnterCombatState(GameStateController obj)
+        {
+            _isCombatActive = true;
+        }
+    }
+}
---
+++ Assets/01_Scripts/Ship/ModuleControllers/ThrusterController.cs
@@ -0,0 +1,10 @@
+﻿using _01_Scripts.Ship.Modules;
+using UnityEngine;
+
+namespace _01_Scripts.Ship.ModuleControllers
+{
+    public class ThrusterController : BaseModuleController
+    {
+        [SerializeField] private ThrusterModuleObject _thrusterModuleObject;
+    }
+}
