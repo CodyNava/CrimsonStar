5f6254393553fce75e5a2ecc052dc6f424e1c32f
Marc Hoffmann <m.h.96@live.de>
2025-04-09 13:51:06 +0200

    [+] Created ScriptableObject for defining AI behaviour

---
+++ Assets/01_Scripts/AI/SimpleAI/SimpleAIParameters.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _01_Scripts.AI.SimpleAI
+{
+    [CreateAssetMenu(fileName = "SimpleAIObject", menuName = "SimpleAI/SimpleAIObject", order = 1)]
+    public class SimpleAIParameters : ScriptableObject
+    {
+        public float wanderDistanceMax;
+        public float wanderDistanceMin;
+        public float wanderReachDistanceMax;
+        public float wanderReachDistanceMin;
+        public float perceptionDistance;
+        public float attackDistance;
+        public float safeDistance;
+    }
+}
