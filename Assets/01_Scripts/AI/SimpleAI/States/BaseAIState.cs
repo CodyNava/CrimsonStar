1fe651a72704a7403326e8967c2225e881c2ef16
Marc Hoffmann <m.h.96@live.de>
2025-04-09 13:51:23 +0200

    [+] Created SimpleAI StateMachine

---
+++ Assets/01_Scripts/AI/SimpleAI/States/BaseAIState.cs
@@ -0,0 +1,33 @@
+﻿namespace _01_Scripts.AI.SimpleAI.States
+{
+    public abstract class BaseAIState
+    {
+        private AIStateMachine _aiStateMachine;
+        protected AIStateMachine AIStateMachineCtx => _aiStateMachine;
+
+        public void EnterState(AIStateMachine aiStateMachine)
+        {
+            _aiStateMachine = aiStateMachine;
+            OnEnterState();
+        }
+        protected abstract void OnEnterState();
+
+        public void UpdateState(float deltaTime)
+        {
+            OnUpdateState(deltaTime);
+        }
+        protected abstract void OnUpdateState(float deltaTime);
+
+        public void FixedUpdateState(float deltaTime)
+        {
+            OnFixedUpdateState(deltaTime);
+        }
+        protected abstract void OnFixedUpdateState(float deltaTime);
+
+        public void ExitState()
+        {
+            OnExitState();
+        }
+        protected abstract void OnExitState();
+    }
+}
---
+++ Assets/01_Scripts/AI/SimpleAI/States/Patrol_AIState.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+namespace _01_Scripts.AI.SimpleAI.States
+{
+    public class Patrol_AIState : BaseAIState
+    {
+        private bool _hasActiveWanderDestination = false;
+        private Vector2 _wanderDestination;
+        private float _wanderReachDistance;
+        
+        protected override void OnEnterState()
+        { }
+
+        protected override void OnUpdateState(float deltaTime)
+        {
+            // TODO: Move towards wanderPosition
+        }
+
+        protected override void OnFixedUpdateState(float deltaTime)
+        {
+            float distanceTowardsWanderPoint =
+                GetDistanceToWanderPosSqr(_wanderDestination, AIStateMachineCtx.transform.position);
+            
+            if (!_hasActiveWanderDestination || distanceTowardsWanderPoint < _wanderReachDistance * _wanderReachDistance)
+            {
+                SetNewWanderTarget();
+            }
+            
+            float distancePlayerSqr = AIStateMachineCtx.GetTargetDistanceSqr();
+            float perceptionDistance = AIStateMachineCtx.AIParameters.perceptionDistance;
+            if (distancePlayerSqr < perceptionDistance * perceptionDistance)
+            {
+                AIStateMachineCtx.SetTransitionState(new Chase_AIState());
+            }
+        }
+
+        protected override void OnExitState()
+        { }
+
+        private void SetNewWanderTarget()
+        {
+            float wanderDistance = ChooseWanderDistance();
+            Vector2 ownPosition = AIStateMachineCtx.transform.position;
+            _wanderDestination = ChooseWanderPosition(wanderDistance, ownPosition);
+            
+            _wanderReachDistance = ChooseWanderReachDistance();
+
+            _hasActiveWanderDestination = true;
+        }
+
+        private float ChooseWanderReachDistance()
+        {
+            float min = AIStateMachineCtx.AIParameters.wanderReachDistanceMin;
+            float max = AIStateMachineCtx.AIParameters.wanderReachDistanceMax;
+            return Random.Range(min, max);
+        }
+
+        private float ChooseWanderDistance()
+        {
+            float min = AIStateMachineCtx.AIParameters.wanderDistanceMin;
+            float max = AIStateMachineCtx.AIParameters.wanderDistanceMax;
+            return Random.Range(min, max);
+        }
+
+        private Vector2 ChooseWanderPosition(float wanderDistance, Vector2 position)
+        {
+            return Random.insideUnitCircle.normalized * wanderDistance + position;
+        }
+
+        private float GetDistanceToWanderPosSqr(Vector2 wanderPosition, Vector2 position)
+        {
+            Vector2 dir = wanderPosition - position;
+            return dir.sqrMagnitude;
+        }
+    }
+}
