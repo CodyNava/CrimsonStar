b26216244511f071f6354ad3880580babdff595d
MikoHeine <Miko.Heine@games-academy.de>
2025-04-10 16:13:45 +0200

    Added Ui for Editor

--- Assets/01_Scripts/ShipEditor/ShipEditor.cs
+++ Assets/01_Scripts/ShipEditor/ShipEditor.cs
@@ -1,5 +1,7 @@
 using _01_Scripts.GameState;
 using _01_Scripts.GameState.States;
+using _01_Scripts.Ship.ModuleControllers;
+using TMPro;
 using UnityEngine;
 
 public class ShipEditor : MonoBehaviour
@@ -7,6 +9,23 @@ public class ShipEditor : MonoBehaviour
     public GameObject ship;
     public GameObject shipEditor;
     public Camera editCamera;
+    public TextMeshProUGUI shipHealth;
+    public TextMeshProUGUI speed;
+    public Shooting[] shooting;
+    public TextMeshProUGUI weapons;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseShipEditor();
+        }
+        UIShipStats();
+    }
+    private void Start()
+    {
+        weapons.text = $"DPS: 0";
+    }
 
     public void OpenShipEditor()
     {
@@ -21,6 +40,24 @@ public class ShipEditor : MonoBehaviour
         editCamera.orthographic = false;
         shipEditor.SetActive(false);
         GameStateController.Instance.ChangeState(new Combat_GameState());
+    }
+
+    public void UIShipStats()
+    {
+        BridgeController controler = ship.GetComponentInChildren<BridgeController>();
+        shipHealth.text = $"HP: {controler.MaxHp}";
+        speed.text = $"Speed: {controler.MoveSpeedChange:0.00}";
+        shooting = ship.GetComponentsInChildren<Shooting>();
+        if (shooting.Length == 0)
+        {
+            weapons.text = $"DPS: 0";
+        }
+        else
+        {
+            weapons.text = $"DPS: {shooting.Length * 10 * 2 * 2}";
+        }
+
+
     }
 
 }
