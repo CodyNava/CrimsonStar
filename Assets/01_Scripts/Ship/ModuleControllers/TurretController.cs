e025174d341b7a6197b21d92a2e5d7b358a1bd42
Marc Hoffmann <m.h.96@live.de>
2025-04-10 11:45:15 +0200

    [*] Resolved merge conflicts

--- Assets/01_Scripts/Ship/ModuleControllers/TurretController.cs
+++ Assets/01_Scripts/Ship/ModuleControllers/TurretController.cs
@@ -1,9 +1,28 @@
-﻿using _01_Scripts.Ship.Modules;
+﻿using System;
+using _01_Scripts.Ship.Modules;
 using UnityEngine;
 
 namespace _01_Scripts.Ship.ModuleControllers
 {
     public class TurretController : BaseModuleController
     {
+        [SerializeField] private GameObject _turretHead;
+        
+        
+        
+        public Vector2 TargetDestination
+        {
+            get => TargetDestination;
+            set
+            {
+                TargetDestination = value;
+                Vector2 direction = (TargetDestination - (Vector2)transform.position).normalized; 
+                _turretHead.transform.up = direction;
+            }
+        }
+
+        public void triggerShoot()
+        {
+        }
     }
 }
