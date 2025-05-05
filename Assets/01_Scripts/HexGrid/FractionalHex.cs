87fe5a36add7bfb3e600f162f21e4df7b0671614
James <contact@jamesfermont.com>
2025-05-05 12:23:13 +0200

    [+] Hex Grid Classes and Ship Data Structs/SOs

---
+++ Assets/01_Scripts/HexGrid/FractionalHex.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public readonly struct FractionalHex
+{
+    private readonly float _q, _r, _s;
+
+    public FractionalHex(float q, float r, float s)
+    {
+        _q = q;
+        _r = r;
+        _s = s;
+    }
+
+    public HexCoordinate Round()
+    {
+        int qi = (int)(Mathf.Round(_q));
+        int ri = (int)(Mathf.Round(_r));
+        int si = (int)(Mathf.Round(_s));
+        float qD = Mathf.Abs(qi - _q);
+        float rD = Mathf.Abs(ri - _r);
+        float sD = Mathf.Abs(si - _s);
+        if (qD > rD && qD > sD)
+        {
+            qi = -ri - si;
+        }
+        else if (rD > sD)
+        {
+            ri = -qi - si;
+        }
+        else
+        {
+            si = -qi - ri;
+        }
+        return new HexCoordinate(qi, ri, si);
+    }
+    
+    public static FractionalHex Lerp(FractionalHex a, FractionalHex b, float t)
+    {
+        return new FractionalHex(a._q * (1.0f - t) + b._q * t, a._r * (1.0f - t) + b._r * t, a._s * (1.0f - t) + b._s * t);
+    }
+}
---
+++ Assets/01_Scripts/Ship/NetModuleData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ModuleData", menuName = "Module Data")]
+public class ModuleData : ScriptableObject
+{
+    public NetModuleID moduleID;
+    public int cost;
+    public NetModuleBaseStats baseStats;
+
+    public NetEditorModule shipEditorPrefab;
+    public NetGameplayModule gameplayPrefab;
+}
