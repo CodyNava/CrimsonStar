5447d3e77fb4eae32cde8d7cfa5d6d8c76559688
James <contact@jamesfermont.com>
2025-05-06 12:36:56 +0200

    [*] NetEditorModule rotation

--- Assets/01_Scripts/Ship/NetEditorModule.cs
+++ Assets/01_Scripts/Ship/NetEditorModule.cs
@@ -1,6 +1,35 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class NetEditorModule : MonoBehaviour
 {
-    
+    [SerializeField] private NetModuleID moduleID;
+
+    public List<HexCoordinate> LocalCoordinates { get; private set; }
+
+    private void Start()
+    {
+        NetModuleData moduleData = DataProvider.Instance.ModuleDB.ModuleData[moduleID];
+        LocalCoordinates = new List<HexCoordinate>();
+        foreach (Vector3Int localCoordinate in moduleData.LocalModuleCoordinates)
+        {
+            LocalCoordinates.Add(new HexCoordinate(localCoordinate.x, localCoordinate.y, localCoordinate.z));
+        }
+    }
+
+    public void RotateClockwise()
+    {
+        for (int i = 0; i < LocalCoordinates.Count; i++)
+        {
+            LocalCoordinates[i] = LocalCoordinates[i].RotateClockwise();
+        }
+    }
+
+    public void RotateCounterclockwise()
+    {
+        for (int i = 0; i < LocalCoordinates.Count; i++)
+        {
+            LocalCoordinates[i] = LocalCoordinates[i].RotateCounterClockwise();
+        }
+    }
 }
