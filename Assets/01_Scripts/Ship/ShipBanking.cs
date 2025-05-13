2dd75c7390af7d035ea5b2b444ba2f2a13df73d2
Marc Hoffmann <m.h.96@live.de>
2025-05-13 12:32:56 +0200

    [*] Refactored to minimalist dependencies
    Now only needs rigidbody2D of root GameObject
    and Transform of target GameObject to be rotated

--- Assets/01_Scripts/Ship/ShipBanking.cs
+++ Assets/01_Scripts/Ship/ShipBanking.cs
@@ -8,44 +8,31 @@ namespace _01_Scripts.Ship
 {
     public class ShipMovementRoll : MonoBehaviour
     {
-        [SerializeField] private NetMovementController _netMovementController;
-        [SerializeField] private NetworkObject _netObject;
-        private PredictionRigidbody2D _rigidbody2D;
-        [SerializeField] private Transform _visualRoot;
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        [SerializeField] private Transform _targetTransform;
         [SerializeField] private float _maxBankAngle;
         [SerializeField] private float _bankSmoothing;
-
-        private bool isInitialized = false;
+        
         private float _currentBank = 0f;
         
         public void Awake()
         {
-            Assert.IsNotNull(_netMovementController, "NetMovementController Script reference needs to be assigned!");
-            Assert.IsNotNull(_netObject, "NetworkObject reference needs to be assigned!");
-            Assert.IsNotNull(_visualRoot, "VisualRoot Transform reference needs to be assigned");
-        }
-
-        public void Start()
-        {
-            _rigidbody2D = _netMovementController.PredictionRB;
-            isInitialized = true;
+            Assert.IsNotNull(_rigidbody2D, "RigidBody2D reference needs to be assigned!");
+            Assert.IsNotNull(_targetTransform, "Target Transform reference needs to be assigned");
         }
 
         public void LateUpdate()
         {
-            if (!_netObject.IsOwner && _netObject.IsClientInitialized)
-                return;
-
-            float steeringAngle = _rigidbody2D.Rigidbody2D.rotation;
+            float steeringAngle = _rigidbody2D.rotation;
             
-            float angularVel = _rigidbody2D.Rigidbody2D.angularVelocity;
+            float angularVel = _rigidbody2D.angularVelocity;
             float targetBankAngle = Mathf.Clamp(angularVel * _maxBankAngle / 100f, -_maxBankAngle, _maxBankAngle);
             
             _currentBank = Mathf.Lerp(_currentBank, targetBankAngle, Time.deltaTime * _bankSmoothing);
             
             Quaternion steeringRotation = Quaternion.Euler(0f, 0f, steeringAngle);
             Quaternion bankingRotation = Quaternion.Euler(0f, _currentBank, 0f);
-            _visualRoot.localRotation = steeringRotation * bankingRotation;
+            _targetTransform.localRotation = steeringRotation * bankingRotation;
         }
     }
 }
