5f83b2ce60989a0eed902ae4e20a963155934766
Martin <martin.mann.t@gmail.com>
2025-05-13 14:10:05 +0200

    [*] Controler Movement for Turet

--- Assets/01_Scripts/Networking/NetFollowCursor.cs
+++ Assets/01_Scripts/Networking/NetFollowCursor.cs
@@ -1,38 +1,58 @@
-﻿using FishNet.Object;
+﻿using System;
+using FishNet.Object;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NetFollowCursor : NetworkBehaviour
 {
-    private Plane _plane = new Plane(Vector3.back, Vector3.zero);
-    private Camera _camera;
+	private Turet _inputAsset;
+	private Plane _plane = new Plane(Vector3.back, Vector3.zero);
+	private Camera _camera;
+	private Vector2 _input;
 
-    public override void OnStartClient()
-    {
-        if (IsOwner)
-        {
-            _camera = Camera.main;
-        }
-        else
-        {
-            enabled = false;
-        }
-    }
+	public override void OnStartClient()
+	{
+		if (IsOwner)
+		{
+			_camera = Camera.main;
+			_inputAsset.Enable();
+		}
+		else
+		{
+			enabled = false;
+			_inputAsset.Disable();
+		}
+	}
 
-    private void Update()
-    {
-        C_LookAtMouse();
-    }
+	private void Awake()
+	{
+		_inputAsset = new Turet();
+	}
+
+	private void Update()
+	{
+		_input = _inputAsset.Player.Look.ReadValue<Vector2>();
+		
+		if (_input.magnitude > 0.2f && Gamepad.current != null)
+		{
+			transform.up = _input.normalized;
+		}
+		else if (Gamepad.current == null)
+		{
+			C_LookAtMouse();
+		}
+	}
 	
-    private void C_LookAtMouse()
-    {
-        if (!_camera) return;
+	private void C_LookAtMouse()
+	{
+		if (!_camera) return;
         
-        Vector3 mouseScreenPos = Input.mousePosition;
-        Ray ray = _camera.ScreenPointToRay(mouseScreenPos);
-        _plane.Raycast(ray, out float distance);
+		Vector3 mouseScreenPos = Input.mousePosition;
+		Ray ray = _camera.ScreenPointToRay(mouseScreenPos);
+		_plane.Raycast(ray, out float distance);
 	
-        Vector3 mouseWorldPos = ray.GetPoint(distance);
-        Vector2 direction = (mouseWorldPos - transform.position).normalized;
-        transform.up = direction;
-    }
+		Vector3 mouseWorldPos = ray.GetPoint(distance);
+		Vector2 direction = (mouseWorldPos - transform.position).normalized;
+		transform.up = direction;
+	}
 }
