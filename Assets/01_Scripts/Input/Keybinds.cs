77f9a9ac78dc41f7f3892c41efd3afc9f019a448
James <contact@jamesfermont.com>
2025-05-13 21:53:22 +0200

    [+] Added stub for input manager

---
+++ Assets/01_Scripts/Input/Keybinds.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class Keybinds
+{
+    public static GameActions Actions;
+    
+#if UNITY_EDITOR
+    [UnityEditor.InitializeOnLoadMethod]
+#endif
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    public static void Initialize()
+    {
+        Actions = new GameActions();
+    }
+}
