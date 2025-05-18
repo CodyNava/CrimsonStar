6382e0cf74ef19e14ad958642a5cba95444f5361
Miko.Heine <Miko.Heine@games-academy.de>
2025-05-18 23:17:11 +0200

    Increased Spawn Distance Between Players

--- Assets/01_Scripts/Menu/SettingsBehaviour.cs
+++ Assets/01_Scripts/Menu/SettingsBehaviour.cs
@@ -148,5 +148,6 @@ public class SettingsBehaviour : MonoBehaviour
         gammaSlider.value = PlayerPrefs.GetFloat(gammaValue, 0.5f);
         brightnessSlider.value = PlayerPrefs.GetFloat(brightnessValue, 0.5f);
         toggle.isOn = PlayerPrefs.GetInt(vSync, 1) == 1;
+        QualitySettings.vSyncCount = QualitySettings.vSyncCount == 1 ? 0 : 1;
     }
 }
