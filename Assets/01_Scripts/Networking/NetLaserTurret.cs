ffcb35c65742ec5edf7a2b1971e462b27c2f5454
Taro <martin.mann.t@gmail.com>
2025-05-21 15:42:02 +0200

    [*]Laser Turet added
    does only include the logic for the turet and the projectile, not the fully build prefab with visuals and sounds

---
+++ Assets/01_Scripts/Networking/NetLaserTurret.cs
@@ -0,0 +1,87 @@
+﻿using FishNet.Object;
+using UnityEngine;
+using UnityEngine.VFX; 
+public class NetLaserTurret : NetworkBehaviour
+{
+    [SerializeField] private NetLaserTurretData netLaserTurretData;
+    [SerializeField] private NetGameplayModule turretModule;
+    [SerializeField] private Transform spawnTransform;
+    [SerializeField] private VisualEffect muzzleFlash;
+    [SerializeField] private AudioSource shootingSound;
+    private const float MaxPassedTime = 0.3f;
+
+    
+
+    private float _accumulatedTime;
+    private float _cooldownTime;
+    private float _chargeTime;
+    private bool _justShot;
+    
+
+    private void Update()
+    {
+        if (!IsOwner) return;
+        _accumulatedTime += Time.deltaTime;
+        _cooldownTime = Mathf.Min(_accumulatedTime, netLaserTurretData.Cooldown);
+        
+
+        if (_cooldownTime < netLaserTurretData.Cooldown) return;
+
+        if (Keybinds.Actions.Player.Attack.IsPressed())
+        {
+            _chargeTime += Time.deltaTime;
+            print(_chargeTime);
+            if (_chargeTime >= netLaserTurretData.ChargeTime)
+            {
+                _accumulatedTime = 0;
+                _chargeTime = 0;
+                _cooldownTime = 0f;
+                C_ClientFire();
+            }
+        }
+        else
+        {
+            _chargeTime = 0;
+        }
+    }
+
+    private void C_ClientFire()
+    {
+        Vector3 position = spawnTransform.position;
+        Vector3 direction = spawnTransform.up;
+
+        if (!IsHostInitialized)
+        {
+            C_SpawnProjectile(position, direction, 0f);
+        }
+        S_ServerFire(position, direction, TimeManager.Tick);
+        muzzleFlash.Play();
+        shootingSound.Play();
+    }
+
+    private void C_SpawnProjectile(Vector3 position, Vector3 direction, float passedTime)
+    {
+        print("Spawning projectile");
+        NetPredictedProjectileLaser pp = Instantiate(netLaserTurretData.Projectile, position, Quaternion.identity);
+        pp.Initialize(direction, passedTime, turretModule.NetTeamID);
+    }
+
+    [ServerRpc]
+    private void S_ServerFire(Vector3 position, Vector3 direction, uint tick)
+    {
+        float passedTime = (float)TimeManager.TimePassed(tick, false);
+        passedTime = Mathf.Min(MaxPassedTime / 2f, passedTime);
+
+        C_SpawnProjectile(position, direction, passedTime);
+        C_ObserversFire(position, direction, tick);
+    }
+
+    [ObserversRpc(ExcludeOwner = true)]
+    private void C_ObserversFire(Vector3 position, Vector3 direction, uint tick)
+    {
+        float passedTime = (float)TimeManager.TimePassed(tick, false);
+        passedTime = Mathf.Min(MaxPassedTime, passedTime);
+        C_SpawnProjectile(position, direction, passedTime);
+        muzzleFlash.Play();
+    }
+}
