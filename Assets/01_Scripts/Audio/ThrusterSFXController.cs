33e19ebb36414eeb5548cef6af97ede31390bd84
Nils Hähndel <Nils.haehndel@games-academy>
2025-05-22 12:12:51 +0200

    [*] Fixed Thruster Sound

--- Assets/01_Scripts/Audio/ThrusterSFXController.cs
+++ Assets/01_Scripts/Audio/ThrusterSFXController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 public class ThrusterSFXController : MonoBehaviour
 {
     [SerializeField] private FMODUnity.EventReference thrusterSoundEvent;
-    [SerializeField] private InputActionReference thrustInput;
+    [SerializeField] private NetMovementController netMovementController;
     [SerializeField] private float changeSpeed = 2f;
     private float _currentStrength;
 
@@ -24,8 +24,8 @@ public class ThrusterSFXController : MonoBehaviour
 
     private void SetThrusterSFXStrenght()
     {
-        Vector2 input = thrustInput.action.ReadValue<Vector2>();
-        bool isThrusting = input.y > 0.2f;
+        float input = netMovementController.InputThrust;
+        bool isThrusting = input > 0.2f;
         float targetStrenght = isThrusting ? 1f : 0f;
         _currentStrength = Mathf.MoveTowards(_currentStrength, targetStrenght, changeSpeed * Time.deltaTime);
         _thrusterSound.setParameterByName("Thrust", _currentStrength);
@@ -34,7 +34,6 @@ public class ThrusterSFXController : MonoBehaviour
     private void OnDestroy()
     {
         {
-            Debug.Log("Thruster OnDestroy");
             _thrusterSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             _thrusterSound.release();
         }
