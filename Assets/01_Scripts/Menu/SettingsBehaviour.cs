264fed350f8407e9d76cdc901a75ef2bfe73454b
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-06-23 14:19:35 +0200

    Fix Gamma and Brightness not loading on Startup

--- Assets/01_Scripts/Menu/SettingsBehaviour.cs
+++ Assets/01_Scripts/Menu/SettingsBehaviour.cs
@@ -173,5 +173,7 @@ public class SettingsBehaviour : MonoBehaviour
         SFXVolume();
         UIVolume();
         AnnouncerVolume();
+        AdjustBrightness();
+        AdjustGamma();
     }
 }
