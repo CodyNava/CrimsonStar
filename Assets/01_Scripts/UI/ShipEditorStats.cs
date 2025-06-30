799225a8c3d628151623d5a5f2630491ce911095
MikoHeine <Miko.Heine@games-academy.de>
2025-06-30 14:55:51 +0200

    Added Scale to screenSize in ShipEditor || Fixed ThrusterStacking Feature || Fixed RocketProjectileRange Calculations

--- Assets/01_Scripts/UI/ShipEditorStats.cs
+++ Assets/01_Scripts/UI/ShipEditorStats.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using FishNet;
 using TMPro;
 using UnityEngine;
 
@@ -29,7 +30,7 @@ public class ShipEditorStats : MonoBehaviour
     private float _mass;
     private float _thrust;
     private float _acceleration;
-    private float _maneuverablility;
+    private float _maneuverability;
 
     [SerializeField] private NetBridgeConfig netBridge;
 
@@ -43,7 +44,7 @@ public class ShipEditorStats : MonoBehaviour
         _mass = 0;
         _thrust = 0;
         _acceleration = 0;
-        _maneuverablility = 0;
+        _maneuverability = 0;
         _rocketRange = 0;
         _laserRange = 0;
         _turretRange = 0;
@@ -58,68 +59,77 @@ public class ShipEditorStats : MonoBehaviour
             _thrust += modules.ModuleData.BaseStats.thrust;
             _maxSpeed = netBridge.MaxMovementSpeed / (1 + _mass);
             _acceleration = netBridge.BaseMovementSpeed + _thrust / (1 + _mass);
-            _maneuverablility = netBridge.MaxAngularSpeed / (1 + _mass);
+            _maneuverability = netBridge.MaxAngularSpeed / (1 + _mass);
 
             if (modules is NetTurretEditorModule turretModule)
             {
-                float projDmg = turretModule.ModuleScriptableObject.Projectile.baseProjectileObject.ProjectileDamage;
+                var turretProjectile = turretModule.ModuleScriptableObject.Projectile.baseProjectileObject;
+                
+                float projDmg = turretProjectile.ProjectileDamage;
                 float shootingCd = turretModule.ModuleScriptableObject.Cooldown;
+                float projTimer = turretProjectile.ProjectileTimer;
+                float projSpeed = turretProjectile.ProjectileSpeed;
+                
                 _damagePerSecond += projDmg / shootingCd;
-                float projTimer = turretModule.ModuleScriptableObject.Projectile.baseProjectileObject.ProjectileTimer;
-                float projSpeed = turretModule.ModuleScriptableObject.Projectile.baseProjectileObject.ProjectileSpeed;
                 _turretRange = projSpeed * projTimer;
             }
             
-
             if (modules is NetLaserTurretEditorModule laserTurretModule)
             {
-                float projDmg = laserTurretModule.ModuleScriptableObject.Projectile.laserProjectileObject
-                    .ProjectileDamage;
+                var laserProjectile = laserTurretModule.ModuleScriptableObject.Projectile.laserProjectileObject;
+                
+                float projDmg = laserProjectile.ProjectileDamage;
                 float shootingCd = laserTurretModule.ModuleScriptableObject.Cooldown + laserTurretModule.ModuleScriptableObject.ChargeTime;
-                float maxHits = laserTurretModule.ModuleScriptableObject.Projectile.laserProjectileObject.MaxHits;
+                float maxHits = laserProjectile.MaxHits;
+                float maxRangeLaser = laserProjectile.MaxLength;
+                
                 _damagePerSecond += (projDmg * maxHits) / shootingCd;
-                float maxRangeLaser =
-                    laserTurretModule.ModuleScriptableObject.Projectile.laserProjectileObject.MaxLength;
                 _laserRange = maxRangeLaser;
             }
             
-
             if (modules is NetRocketEditorModule rocketEditorModule)
             {
-                float projDmg = rocketEditorModule.ModuleScriptableObject.Projectile.rocketProjectileObject
-                    .ProjectileDamage;
                 //float explDmg = rocketEditorModule.ModuleScriptableObject.Projectile.rocketProjectileObject.   //todo explosion damage beachten
-                float shootingCd = rocketEditorModule.ModuleScriptableObject.Cooldown;
+                var rocketProjectile = rocketEditorModule.ModuleScriptableObject.Projectile.rocketProjectileObject;
+                
+                float projDmg = rocketProjectile.ProjectileDamage;
+                float shootingCd = rocketEditorModule.ModuleScriptableObject.Cooldown; 
+                float rocketTimer = rocketProjectile.ProjectileTimer;
+                float dt = (float)InstanceFinder.TimeManager.TickDelta;
+                int numberOfTicks = Mathf.FloorToInt(rocketTimer / dt);
+                float distanceTraveled = 0f;
+                float velocity = 0f;
+                float rocketSpeed = rocketProjectile.ProjectileAcceleration;
+                for (int i = 0; i < numberOfTicks; i++)
+                {
+                    velocity += rocketSpeed * dt;
+                    distanceTraveled += velocity * dt;
+                }
+                
+                float rocketRangeUnrounded = distanceTraveled / 10;
                 _damagePerSecond += projDmg / shootingCd;
-                float rocketTimer = rocketEditorModule.ModuleScriptableObject.Projectile.rocketProjectileObject
-                    .ProjectileTimer;
-                float rocketSpeed = rocketEditorModule.ModuleScriptableObject.Projectile.rocketProjectileObject
-                    .ProjectileAcceleration;
-                _rocketRange = rocketSpeed * rocketTimer;
+                _rocketRange = Mathf.RoundToInt(rocketRangeUnrounded) * 10;
+                
             }
-            
-            
             
             List<float> validValues = new List<float> { _rocketRange, _turretRange, _laserRange }.Where(v => v != 0f).ToList();
             _maxRange = validValues.Count > 0 ? validValues.Max() : 0f;
             _minRange = validValues.Count > 0 ? validValues.Min() : 0f;
         }
-
+        
         DisplayStats();
-        
-        
         // todo: make the speed calcuations more clean by getting the value directly from NetBridge
     }
 
     public void DisplayStats()
     {
-        totalHealth.text = $"TotalHealth: {_totalHealth:0.0}";
-        hexCount.text = $"HexCount: {_hexCount}";
-        damagePerSecond.text = $"DmgPerSecond: {_damagePerSecond:0.0}";
+        totalHealth.text = $"TotalHealth: {_totalHealth:0}";
+        hexCount.text = $"HexCount: {_hexCount:0}";
         maxSpeed.text = $"MaxSpeed: {_maxSpeed:0.0}";
         acceleration.text = $"Acceleration: {_acceleration:0.0}";
-        maneuverability.text = $"Maneuverablility: {_maneuverablility:0.0}";
-        maxRange.text = $"MaxRange: {_maxRange:0.0}";
-        minRange.text = $"MinRange: {_minRange:0.0}";
+        maneuverability.text = $"Maneuverability: {_maneuverability:0.0}";
+        damagePerSecond.text = $"DmgPerSecond: {_damagePerSecond:0}";
+        maxRange.text = $"MaxRange: {_maxRange:0}";
+        minRange.text = $"MinRange: {_minRange:0}";
     }
 }
