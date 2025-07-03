e4bc82977abda12cd6ab7f312be4816e3c6112db
Nils Hähndel <Nils.haehndel@games-academy>
2025-07-03 20:05:14 +0200

    [*] Spacial Audio on Explosions now should work

--- Assets/01_Scripts/Audio/SFXDetonate.cs
+++ Assets/01_Scripts/Audio/SFXDetonate.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SFXDetonate : MonoBehaviour
 {
-    [SerializeField] private FMODUnity.EventReference detonateSound;
+    [SerializeField] private FMODUnity.StudioEventEmitter ExplosionEvent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
