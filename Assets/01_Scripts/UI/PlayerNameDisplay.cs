fec19cb24c2fda904c17b82249a5e0d050e4ad1a
MikoHeine <Miko.Heine@games-academy.de>
2025-07-07 17:48:33 +0200

    Added Team Colours & FreeForAll Colors

--- Assets/01_Scripts/UI/PlayerNameDisplay.cs
+++ Assets/01_Scripts/UI/PlayerNameDisplay.cs
@@ -18,8 +18,37 @@ public class PlayerNameDisplay : MonoBehaviour
 
     public void DisplayName()
     {
-        bool inTeamOne = bridgeModule.NetTeamID == NetTeamID.Team1;
-        nameDisplay.color = inTeamOne ? Color.green : Color.red;
+        //bool inTeamOne = bridgeModule.NetTeamID == NetTeamID.Team1;
+
+        switch (bridgeModule.NetTeamID)
+        {
+            case NetTeamID.Team1:
+                nameDisplay.color = Color.blue;
+                break;
+            case NetTeamID.Team2:
+                nameDisplay.color = Color.red;
+                break;
+            case NetTeamID.Team3:
+                nameDisplay.color = Color.cyan;
+                break;
+            case NetTeamID.Team4:
+                nameDisplay.color = Color.green;
+                break;
+            case NetTeamID.Team5:
+                nameDisplay.color = Color.yellow;
+                break;
+            case NetTeamID.Team6:
+                nameDisplay.color = Color.magenta;
+                break;
+            case NetTeamID.Team7:
+                nameDisplay.color = Color.white;
+                break;
+            case NetTeamID.Team8:
+                nameDisplay.color = Color.grey;
+                break;
+            
+        }
+        //nameDisplay.color = inTeamOne ? Color.green : Color.red;
         string name = bridge.DisplayName;
         nameDisplay.text = $"{name}";
     }
