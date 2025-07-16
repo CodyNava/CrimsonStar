2474e378f691a911e909047a7d256aee27e4216c
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-07-16 13:45:44 +0200

    Working Out of Bounds Feature
    Damage currently credited to Out of Bounds Player

--- Assets/01_Scripts/Networking/NetOutOfBounds.cs
+++ Assets/01_Scripts/Networking/NetOutOfBounds.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 public class NetOutOfBounds : MonoBehaviour
 {
     private NetGameplayModule _module;
+    private Coroutine _destruction;
+    private bool _destructionTriggered;
 
-    private IEnumerator OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<NetGameplayModule>().ModuleID != NetModuleID.Bridge) yield break;
+        if (other.gameObject.GetComponent<NetGameplayModule>().ModuleID != NetModuleID.Bridge)
+        {
+            return;
+        }
+        _destruction = StartCoroutine(DestructionCoroutine(other));
+    }
+
+    private IEnumerator DestructionCoroutine(Collider2D other)
+    {
+        Debug.Log("Destruction Started");
         _module = other.gameObject.GetComponent<NetGameplayModule>();
         var i = 10f;
         while (i > 0f)
@@ -15,11 +26,19 @@ public class NetOutOfBounds : MonoBehaviour
             yield return new WaitForSeconds(1f);
             i--;
         }
+        _destructionTriggered = true;
         _module.S_InflictDamage(9999, _module.Bridge.PlayerID);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        StopCoroutine(OnTriggerEnter2D(other));
+        if (_destructionTriggered) return;
+        if (_destruction != null)
+        {
+            StopCoroutine(_destruction);
+            _destruction = null;
+        }
+        Debug.Log("Stopped");
+        _destructionTriggered = false;
     }
-}
+}   
