72bc4f7110a7c8b53fd1ba3ebf0391996f13dc01
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-08-06 13:18:23 +0200

    Readystate for PlayerPlates

--- Assets/01_Scripts/UI/PlayerPlateDisplay.cs
+++ Assets/01_Scripts/UI/PlayerPlateDisplay.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class PlayerPlateDisplay : MonoBehaviour
 {
-    //[SerializeField] private Image readyImage;
+    [SerializeField] private Sprite readySprite;
+    [SerializeField] private Sprite unreadySprite;
+    [SerializeField] private Image background;
     [SerializeField] private TMP_Text playerName;
     private NetLobbyData _lobbyData;
 
@@ -13,6 +15,7 @@ public class PlayerPlateDisplay : MonoBehaviour
     {
         _lobbyData = lobbyData;
         //readyImage.gameObject.SetActive(lobbyData.isReady);
+        background.sprite = lobbyData.isReady ? readySprite : unreadySprite;
         playerName.text = lobbyData.playerDisplayName;
         bool canEdit = lobbyData.playerID == PlayerData.PlayerID || PlayerData.IsLobbyHost;
         /*if (canEdit)
@@ -40,7 +43,7 @@ public class PlayerPlateDisplay : MonoBehaviour
 
     public void ResetDisplay()
     {
-        //readyImage.gameObject.SetActive(false);
+        background.sprite = unreadySprite;
         playerName.text = "-";
     }
 }
