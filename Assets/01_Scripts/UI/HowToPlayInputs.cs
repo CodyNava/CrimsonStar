048ab931271e1103252481099159aaeba769fd0d
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-08-11 20:27:50 +0200

    [*] HowToPlay without Gifs implemented

--- Assets/01_Scripts/UI/HowToPlayInputs.cs
+++ Assets/01_Scripts/UI/HowToPlayInputs.cs
@@ -1,64 +1,82 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class HowToPlayInputs : MonoBehaviour
 {
     [SerializeField] private GameObject howToPlayContainer;
-    [SerializeField] private Button rules;
-    [SerializeField] private GameObject rulesContainer;
-    [SerializeField] private Button modules;
-    [SerializeField] private GameObject modulesContainer;
+    [SerializeField] private Button howToPlay;
+    [SerializeField] private GameObject htpContainer;
+    [SerializeField] private Button editor;
+    [SerializeField] private GameObject editorContainer;
     [SerializeField] private Button hazards;
     [SerializeField] private GameObject hazardsContainer;
     [SerializeField] private Button icons;
     [SerializeField] private GameObject iconsContainer;
     [SerializeField] private Button htpBack;
+
+    [SerializeField] private List<GameObject> buttonPrompts;
     
     private void Update()
     {
+        foreach (var prompt in buttonPrompts)
+        {
+            prompt.SetActive(InputManager.Instance.IsGamepadUsed);
+        }
         if (Keybinds.Actions.UI.SwapTabLeft.WasPressedThisFrame())
         {
-            if (rulesContainer.activeSelf)
+            
+            if (htpContainer.activeSelf)
             {
+                EventSystem.current.SetSelectedGameObject(null);
                 icons.onClick.Invoke();
             }
-
-            else if (modulesContainer.activeSelf)
+            
+            else if (editorContainer.activeSelf)
             {
-                rules.onClick.Invoke();
+                EventSystem.current.SetSelectedGameObject(null);
+                howToPlay.onClick.Invoke();
             }
 
             else if (hazardsContainer.activeSelf)
             {
-                modules.onClick.Invoke();
+                EventSystem.current.SetSelectedGameObject(null);
+                editor.onClick.Invoke();
             }
 
             else if (iconsContainer.activeSelf)
             {
+                EventSystem.current.SetSelectedGameObject(null);
                 hazards.onClick.Invoke();
             }
         }
 
         if (Keybinds.Actions.UI.SwapTabRight.WasPressedThisFrame())
         {
-            if (rulesContainer.activeSelf)
+            if (htpContainer.activeSelf)
             {
-                modules.onClick.Invoke();
+                EventSystem.current.SetSelectedGameObject(null);
+                editor.onClick.Invoke();
+            }
+            
+            else if (editorContainer.activeSelf)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+               hazards.onClick.Invoke();
             }
 
-            else if (modulesContainer.activeSelf)
-            {
-                hazards.onClick.Invoke();
-            }
 
             else if (hazardsContainer.activeSelf)
             {
+                EventSystem.current.SetSelectedGameObject(null);
                 icons.onClick.Invoke();
             }
 
             else if (iconsContainer.activeSelf)
             {
-                rules.onClick.Invoke();
+                EventSystem.current.SetSelectedGameObject(null);
+                howToPlay.onClick.Invoke();
             }
         }
 
